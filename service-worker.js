27946e951f17fe72487c8112cc92e95e00b2b1f4
sethesh2125-ops <sethesh2125@gmail.com>
2025-11-06 16:38:23 +0530

    unfinished demo

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const cacheName = "ai-tutor-cache-v1";
-const assets = ["index.html", "main.js", "lesson1.mp3"];
+const assets = ["index.html", "main.js", "class6_math_unit1.mp3","class6_math_unit2.mp3"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
